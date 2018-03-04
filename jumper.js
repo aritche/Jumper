181103bb64cc5498453079608dee7d9be7ece69a
Ari <ari.tchetchenian@gmail.com>
2018-03-04 16:52:26 +1100

    Basic jumping functionality

--- jumper.js
+++ jumper.js
@@ -5,16 +5,87 @@ var requestAnimationFrame = window.requestAnimationFrame ||
 
 var canvas = createCanvas(500, 250, 'white', 'gameArea');
 var ctx = canvas.getContext("2d");
+
+var players = [];
+var stage;
+var g = -5;
+
 function main(){
+    players.push(new Player("Player A", 0, "white"));
+    players.push(new Player("Player B", 1, "green"));
+    
+    stage = new Stage(canvas.width*0.15,
+                      canvas.height*0.9,
+                      canvas.width*0.70,
+                      canvas.height*0.1, 
+                      "black");
+    placePlayers();
     updateCanvas();
 }
+
+function placePlayers(){
+    for (var p = 0; p < players.length; p++){
+        players[p].x = stage.x+players[p].radius+stage.width*(p/(players.length-1));
+        
+        //fix final player's position
+        if (p == players.length-1){
+            players[p].x -= players[p].radius*2;
+        }
+        players[p].y = canvas.height-(stage.height+players[p].radius);
+
+        // sink player slightly into ground
+        players[p].y += players[p].radius*2*0.08;
+    }
+}
+
+function Stage(x, y, width, height, color){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+}
+
+
+// direction 'n' = neutral
+function Player(name, id, color){
+    this.name = name;
+    this.id = id;
+    this.color = color;
+    this.x = 0;
+    this.y = 0;
+
+    this.direction = 'n'
+
+    this.radius = 20;
+
+    this.velocity = [0,0]; // velocity x and y
+
+    this.move = function(direction){
+        this.direction = direction;
+    }
+
+    this.jumping = false;
+    this.jump = function(){
+        this.velocity[1] = 100;
+        this.jumping = true;
+    }
+}
+
+function checkKey(e){
+    e = e || window.event;
+    if (e.keyCode == '32') players[0].jump();
+}
+document.onkeydown = checkKey;
 
 function createCanvas(w, h, color, id){
     var canvas = document.createElement('canvas');
     canvas.id = id;
     canvas.className = 'canvas';
-    canvas.width = w;
-    canvas.height = h;
+    canvas.width = w*2; //resolution
+    canvas.height = h*2; //resolution
+    canvas.style.width = w; //actual width
+    canvas.style.height = h; //actual height
     canvas.style.backgroundColor = color;
     canvas.style.marginBottom = '20px';
     canvas.style.marginTop = '20px';
@@ -25,18 +96,34 @@ function createCanvas(w, h, color, id){
 
 function updateCanvas(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
+    
+    movePlayers();
 
     paintBackdrop();
     paintDanger();
-    paintFloor();
+    paintPlayers();
+    paintStages();
 
     requestAnimationFrame(updateCanvas);
 }
 
-function paintFloor(){
+function movePlayers(){
+    for (var p = 0; p < players.length; p++){
+        players[p].y -= players[p].velocity[1];
+        players[p].x -= players[p].velocity[0];
+        if (players[p].y > stage.y-players[p].radius){
+            players[p].jumping = false;
+            players[p].velocity[1] = 0;
+        } else{
+            players[p].velocity[1] =+ g;
+        }
+    }
+}
+
+function paintStages(){
     ctx.beginPath();
-    ctx.fillStyle="black";
-    ctx.fillRect(canvas.width*0.15,canvas.height * 0.8, canvas.width*0.7, canvas.height);
+    ctx.fillStyle=stage.color;
+    ctx.fillRect(stage.x, stage.y, stage.width, stage.height);
     ctx.closePath();
 }
 
@@ -54,8 +141,14 @@ function paintDanger(){
     ctx.closePath();
 }
 
-function paintPlayer(){
-
+function paintPlayers(){
+    for (var p = 0; p < players.length; p++){
+        ctx.beginPath();
+        ctx.arc(players[p].x,players[p].y,players[p].radius,0,2*Math.PI);
+        ctx.fillStyle = players[p].color;
+        ctx.fill();
+        ctx.closePath();
+    }
 }
 
 main();
